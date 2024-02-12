712199e3dc3ebe1dd922ea64af7849484ba79db1
CUMALİ GÜZEL <cumali.guzel@std.yeditepe.edu.tr>
2024-02-12 11:30:41 +0300

    Simple UI
    Simple UI for pizza sale

---
+++ app/src/main/java/com/cumaliguzel/androidstart/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.cumaliguzel.androidstart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //hatırla bu büyük R harfi ne demek diye sormuştum sana R res klasörünü temsil eder
+        //Xml kısmı @işareti res dosyasını işaret ediyor
+        //left-Start-leading(ios)
+
+
+    }
+}
